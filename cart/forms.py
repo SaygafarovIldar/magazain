80063cbdb7938685b917b4914db441ca122a1bb1
microman92 <90110834+microman92@users.noreply.github.com>
2023-05-15 20:59:29 +0500

    asmdfijfa

---
+++ cart/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Customer, ShippingAddress
+
+
+class CustomerForm(forms.ModelForm):
+    pass
+
+
+class ShippingAddressForm(forms.ModelForm):
+    pass
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import Product, Category
+from django.core.paginator import Paginator
 # Create your views here.
 
 
@@ -13,8 +14,12 @@ def home_view(request):
 
 def shop_view(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
 
@@ -22,8 +27,12 @@ def shop_view(request):
 def category_products(request, slug):
     category = Category.objects.get(slug=slug)
     products = Product.objects.filter(category=category)
+    paginator = Paginator(products, 1)
+    page = request.GET.get("page")
+    result = paginator.get_page(page)
+
     context = {
-        "products": products
+        "products": result
     }
     return render(request, "pages/shop.html", context)
 
